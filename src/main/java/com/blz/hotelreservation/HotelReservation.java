74130fdc849e21de36b58d6a610905ee3a48a558
KarthikBenki <karthikmc007@gmail.com>
2022-03-14 09:00:15 +0530

    Added: add hotel method

--- src/main/java/com/blz/hotelreservation/HotelReservation.java
+++ src/main/java/com/blz/hotelreservation/HotelReservation.java
@@ -1,10 +1,18 @@
 package com.blz.hotelreservation;
 
 public class HotelReservation {
+	
+public Hotel addHotel(String hName,double weekDayRate,double weekEndRate ) {
+		Hotel hotel = new Hotel();
+		hotel.setHotelName(hName);
+		hotel.setRateForRegularCustomerWeekDay(weekDayRate);
+		hotel.setRateForRegularCustomerWeekEnd(weekEndRate);
+		return hotel;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Hotel Reservation System");
 
 	}
-
+		
 }
